8af0e9a958392a865b583451f2d1aa416d4ab5ab
jhaydraude <jeremy.haydraude@gmail.com>
2019-09-19 10:51:00 -0400

    Use ISVTE Plugin

--- orgInit.sh
+++ orgInit.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-sfdx force:org:create -f config/project-scratch-def.json -a AppAnalytics  --setdefaultusername -d 1
+sfdx force:source:convert -d mdapioutput
 
-#Install LMA Package
-sfdx force:package:install -p 04t30000001DWL0 -w 20
-
-#Push Source
-sfdx force:source:push -f
-
-#Grant Permission
-sfdx force:user:permset:assign -n AppAnalytics
-
-#Open Org
-sfdx force:org:open -p lightning/n/App_Analytics_Query_Requests
+sfdx isvte:mdscan -d mdapioutput
